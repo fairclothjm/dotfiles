c1de73ce1e0705ea608b1f37a483d5bd0b63e2cc
JM Faircloth <fairclothjm@gmail.com>
2020-08-07 11:57:23 -0500

    add git configs to setup script and make it fancy

--- pi/symlink.sh
+++ pi/symlink.sh
@@ -1,9 +1,49 @@
 #!/usr/bin/env bash
 
-for file in `ls $(pwd)`; do
-    if [[ "$file" == `basename "$0"` ]]; then
-        continue
+#
+#; script to symlink config files
+#; run this from the pi dir
+#
+
+if [[ -z $TERM ]]; then
+    $TERM="xterm"
+fi
+
+# do not symlink these
+readonly -a skip=(`basename "$0"` "README.md")
+
+readonly dir_path="/home/pi/code/dotfiles"
+readonly pi_path="${dir_path}/pi"
+readonly git_path="${dir_path}/git"
+
+# fancy color for errors
+readonly txtred="$(tput -T $TERM setaf 1)"
+readonly txtreset="$(tput -T $TERM sgr0)"
+
+check_code() {
+    local code=$1
+    local file=$2
+
+    if [[ "$code" -ne 0 ]]; then
+        echo "${txtred}[error]${txtreset} could not symlink $file"
     fi
-    echo "Creating symlink to $file"
-    ln -sf $(pwd)/$file ~/.$file
+}
+
+
+for file in `ls ${pi_path}`; do
+    if [[ ! " ${skip[@]} " =~ " ${file} " ]]; then
+        echo "creating symlink to $file"
+        ln -sf $pi_path/$file ~/.$file
+        check_code $? $file
+    fi
 done
+
+
+echo "setting up git configs"
+for file in `ls ${git_path}`; do
+    echo "creating symlink to $file"
+    ln -sf $git_path/$file ~/.$file
+    check_code $? $file
+done
+
+ls -lah --color=always ~
